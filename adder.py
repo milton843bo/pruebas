847ddfb24def62861c4d7e9d7c3a5eedba95c817
milton843bo <espinoza.milton@gmail.com>
2023-12-18 16:45:40 -0400

    first commit

---
+++ adder.py
@@ -0,0 +1,13 @@
+# adder.py
+
+import unittest
+
+def add(a: int, b: int) -> int:
+    return a + b
+
+class TestAdder(unittest.TestCase):
+    def test_add_adds_two_numbers(self):
+        self.assertEqual(add(1, 2), 3)
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ echo_demo.py
@@ -0,0 +1,12 @@
+# echo_demo.py
+
+def echo(text: str, repetitions: int = 3) -> str:
+    """Imitate a real-world echo."""
+    echoed_text = ""
+    for i in range(repetitions, 0, -1):
+        echoed_text += f"{text[-i:]}\n"
+    return f"{echoed_text.lower()}."
+
+if __name__ == "__main__":
+    print('Example call: echo("HELLO", repetitions=2)', end=f"\n{'-' * 42}\n")
+    print(echo("HELLO", repetitions=2))
